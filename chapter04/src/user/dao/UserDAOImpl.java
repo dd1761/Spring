6517b076ad42b2350a7a1c01488926e9fe28f74e
KIMDONGBEEN <dd1761@naver.com>
2023-04-07 17:49:04 +0900

    23-04-07

--- chapter04/src/user/dao/UserDAOImpl.java
+++ chapter04/src/user/dao/UserDAOImpl.java
@@ -1,23 +1,22 @@
 package user.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import lombok.Setter;
 import user.bean.UserDTO;
 
+/*
 public class UserDAOImpl implements UserDAO{
 	@Setter
 	private JdbcTemplate jdbcTemplate = null;
 	
-
-//	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-//		this.jdbcTemplate = jdbcTemplate;
-//	}
-
-
 	@Override
 	public void write(UserDTO userDTO) {
 		String sql = "insert into usertable values(?,?,?)";
@@ -33,4 +32,35 @@ public class UserDAOImpl implements UserDAO{
 		//RowMapper은 한줄의 데이터값 중 하나의 데이터들으 한개의 데이터와 매핑하여 넣어준다.
 	}
 
+}*/
+
+
+public class UserDAOImpl extends NamedParameterJdbcDaoSupport implements UserDAO{
+	//@Setter
+	//private JdbcTemplate jdbcTemplate = null;
+	//JdbcDaoSupport 상속하였으므로 jdbcTemplate를 빈으로 생성할 필요가 없다.
+	
+	
+	@Override
+	public void write(UserDTO userDTO) { //NamedParameterJdbcDaoSupport는 Map으로만 받아야한다.
+		String sql = "insert into usertable values(:name,:id,:pwd)";
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", userDTO.getName());
+		map.put("id", userDTO.getId());
+		map.put("pwd", userDTO.getPwd());
+		
+		getNamedParameterJdbcTemplate().update(sql, map);
+		
+	}
+
+
+	@Override
+	public List<UserDTO> getUserList() {
+		String sql = "select * from usertable";
+		return getJdbcTemplate().query(sql, new BeanPropertyRowMapper<UserDTO>(UserDTO.class)); 
+		//RowMapper은 한줄의 데이터값 중 하나의 데이터들으 한개의 데이터와 매핑하여 넣어준다.
+	}
+
 }
+
---
+++ chapter04/src/user/service/UserUpdateService.java
@@ -0,0 +1,24 @@
+package user.service;
+
+import java.util.Scanner;
+
+public class UserUpdateService implements UserService {
+
+	@Override
+	public void execute() {
+		//수정 할 아이디 입력 : ???;
+		//찾고자 하는 아이디가 없습니다.
+		
+		//수정할 아이디 입력 : hong;
+		//이름	아이디	패스워드
+		//???	hong	111
+		
+		//수정할 이름 입력 : ???
+		//수정할 비번 입력 : ???
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+	}
+
+}
