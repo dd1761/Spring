c29e5aa0e55274309e1af8203203314d828a9fc5
KIMDONGBEEN <dd1761@naver.com>
2023-04-10 18:09:08 +0900

    23-04-10

---
+++ chapter06/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value="/hello.do", method=RequestMethod.GET)
+	public ModelAndView hello() { //사용자 콜백 메소드
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("result", "Hello Spring !!"); //아래의 request와 같은 것.
+		//request.setAttribute("result", "Hello Spring !!");
+		mav.setViewName("hello");	//파일명 지정
+		
+		return mav;
+	}
+}
+
+
+/*
+
+콜백 메소드
+- 어떤 때가 되면 운영체제, 스프링에 의해서 호출되는 메소드
+
+http://localhost:8080/Context명(Project명)/hello.do 요청
+
+DispatcherServlet
+		↕ HandlerMapping
+HelloController.java
+
+
+*/
