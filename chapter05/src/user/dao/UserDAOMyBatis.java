d2dc21713c04966b57112c9e6b91f21219b893fe
KIMDONGBEEN <dd1761@naver.com>
2023-04-10 15:32:13 +0900

    23-04-10

---
+++ chapter05/src/user/dao/UserDAOMyBatis.java
@@ -0,0 +1,47 @@
+package user.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.Setter;
+import user.bean.UserDTO;
+
+@Transactional
+public class UserDAOMyBatis implements UserDAO {
+	@Setter 
+	private SqlSession sqlSession;
+
+	//@Transactional //transactional을 사용하면 commit과 close를 자동으로 한다.
+	@Override
+	public void write(UserDTO userDTO) {
+		sqlSession.insert("userSQL.write", userDTO);
+	}
+
+	//@Transactional
+	@Override
+	public List<UserDTO> getUserList() {
+		return sqlSession.selectList("userSQL.getUserList"); //userMapper의 getUserList문을 사용
+	} 
+
+	@Override
+	public UserDTO getUser(String id) {
+		
+		return sqlSession.selectOne("userSQL.getUser", id);
+	}
+
+	@Override
+	public void update(Map<String, String> map) {
+		sqlSession.update("userSQL.update", map);
+
+	}
+
+	@Override
+	public void delete(String id) {
+		sqlSession.delete("userSQL.delete", id);
+
+	}
+
+}
