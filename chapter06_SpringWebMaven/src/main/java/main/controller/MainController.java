8f2cd938e5181931026c5417e95cf2cac78b28b8
KIMDONGBEEN <dd1761@naver.com>
2023-04-11 16:39:22 +0900

    23-04-11

--- chapter06_SpringWebMaven/src/main/java/main/controller/MainController.java
+++ chapter06_SpringWebMaven/src/main/java/main/controller/MainController.java
@@ -1,12 +1,13 @@
 package main.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 	
-	@GetMapping(value="index.do")
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String main() {
 		
 		return "index";
